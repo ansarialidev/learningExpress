cd608a48d035abda7ffd2562e443d4a5afeb4782
ansariali <ansariali.developer@gmail.com>
2025-03-21 20:36:29 +0530

    http module in express js

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-console.log("Http Module");
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+//!http Module in express js
+
+app.use(express.json());
+
+//?performing get request
+app.get("/", (req, res) => {
+  res.send("Welcome back user");
+});
+
+//? performing post request
+app.post("/user", (req, res) => {
+  const { name, email } = req.body;
+  res.json({
+    message: `user ${name} with email ${email} created successfully`,
+  });
+});
+
+//? performing put method
+app.put("/user/:id", (req, res) => {
+  const userID = req.params.id;
+  const { name, email } = req.body;
+  res.json({
+    message: `User ${userID} updated on ${name} ${email}`,
+  });
+});
+
+//? performimg delete method
+app.delete("/user/:id", (req, res) => {
+  const userID = req.params.id;
+  res.json({
+    message: `user with ID ${userID} deleted successfullt`,
+  });
+});
+
+//? performing get method
+app.get("/profile/:name/:id([0-9]{5})", (req, res) => {
+  const { name, id } = req.params;
+  res.json({
+    id,
+    name,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is Running ${PORT}`);
+});
