290010f7201563b2585732e16432a0a2d17c68f0
ansariali <ansariali.developer@gmail.com>
2025-03-21 18:04:00 +0530

    initial commit

---
+++ controller.js
@@ -0,0 +1,15 @@
+export const userLogin = (req, res) => {
+  res.send("this is a user login routes");
+};
+
+export const userSignup = (req, res) => {
+  res.send("this is a user signup routes");
+};
+
+// export const userController = (req, res) => {
+//   res.send(`Welcome ${req.params.username}`);
+// };
+
+// export const userSearch = (req, res) => {
+//   res.send(`Searching for ${req.query.keyword}`);
+// };
